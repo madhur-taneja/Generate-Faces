5526ca7a6e0eddbef07f52650ee4422f926212c1
Madhur Taneja <tanejamadhur96@gmail.com>
2018-06-15 00:06:29 +0530

    Updating helper.py to remove an error

--- helper.py
+++ helper.py
@@ -94,6 +94,35 @@ def get_batch(image_files, width, height, mode):
     return data_batch
 
 
+# def images_square_grid(images, mode):
+#     """
+#     Save images as a square grid
+#     :param images: Images to be used for the grid
+#     :param mode: The mode to use for images
+#     :return: Image of images in a square grid
+#     """
+#     # Get maximum size for square grid of images
+#     save_size = math.floor(np.sqrt(images.shape[0]))
+
+#     # Scale to 0-255
+#     images = (((images - images.min()) * 255) / (images.max() - images.min())).astype(np.uint8)
+
+#     # Put images in a square arrangement
+#     images_in_square = np.reshape(
+#             images[:save_size*save_size],
+#             (save_size, save_size, images.shape[1], images.shape[2], images.shape[3]))
+#     if mode == 'L':
+#         images_in_square = np.squeeze(images_in_square, 4)
+
+#     # Combine images to grid image
+#     new_im = Image.new(mode, (images.shape[1] * save_size, images.shape[2] * save_size))
+#     for col_i, col_images in enumerate(images_in_square):
+#         for image_i, image in enumerate(col_images):
+#             im = Image.fromarray(image, mode)
+#             new_im.paste(im, (col_i * images.shape[1], image_i * images.shape[2]))
+
+#     return new_im
+
 def images_square_grid(images, mode):
     """
     Save images as a square grid
@@ -103,26 +132,21 @@ def images_square_grid(images, mode):
     """
     # Get maximum size for square grid of images
     save_size = math.floor(np.sqrt(images.shape[0]))
-
     # Scale to 0-255
     images = (((images - images.min()) * 255) / (images.max() - images.min())).astype(np.uint8)
-
     # Put images in a square arrangement
     images_in_square = np.reshape(
             images[:save_size*save_size],
             (save_size, save_size, images.shape[1], images.shape[2], images.shape[3]))
     if mode == 'L':
         images_in_square = np.squeeze(images_in_square, 4)
-
     # Combine images to grid image
     new_im = Image.new(mode, (images.shape[1] * save_size, images.shape[2] * save_size))
     for col_i, col_images in enumerate(images_in_square):
         for image_i, image in enumerate(col_images):
             im = Image.fromarray(image, mode)
             new_im.paste(im, (col_i * images.shape[1], image_i * images.shape[2]))
-
-    return new_im
-
+    return new_im.convert('RGBA')
 
 def download_extract(database_name, data_path):
     """
